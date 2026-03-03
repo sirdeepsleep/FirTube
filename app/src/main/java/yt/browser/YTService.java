f09591506d7cedb1b701c70276382e41bfef935f
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-03 11:55:54 +0500

    Update YTService.java

--- app/src/main/java/yt/browser/YTService.java
+++ app/src/main/java/yt/browser/YTService.java
@@ -49,8 +49,9 @@ public class YTService extends Service {
                 "  const selectors = ['.ytp-ad-overlay-container', '.ytp-ad-message-container', 'ytm-companion-ad-renderer', 'ytm-promoted-sparkles-web-renderer', '.ad-unit', 'ytm-promoted-video-renderer'];" +
                 "  selectors.forEach(selector => {" +
                 "    const el = document.querySelector(selector);" +
+                "  if ("+ZeroActivity.banerBlock+"==true){"+       
                 "    if (el) el.remove();" +
-                "  });" +
+                "  }});" +
                 "}, 200);", null);
         }
     });
