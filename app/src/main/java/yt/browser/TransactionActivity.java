01be6932dd6b0fef7608669178beda0cd2b7166c
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-03 07:12:43 +0500

    Update TransactionActivity.java

--- app/src/main/java/yt/browser/TransactionActivity.java
+++ app/src/main/java/yt/browser/TransactionActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.*;
 
 public class TransactionActivity extends Activity {
 
