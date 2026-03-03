fe471d419dc782f27f05769e8697268c86b0d885
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-03 12:56:42 +0500

    Update ZeroActivity.java

--- app/src/main/java/yt/browser/ZeroActivity.java
+++ app/src/main/java/yt/browser/ZeroActivity.java
@@ -27,6 +27,12 @@ public class ZeroActivity extends Activity {
        @Override
     protected void onResume() {
         super.onResume();
+        if (continued) {
+        Intent i = new Intent(this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            wait=false;
+            startActivity(i);
+        }
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE | 
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | 
