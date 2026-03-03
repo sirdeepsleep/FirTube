52a3095985bc3c929ea853c002906dd0ff3cdece
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-03 18:58:36 +0500

    Update ZeroActivity.java

--- app/src/main/java/yt/browser/ZeroActivity.java
+++ app/src/main/java/yt/browser/ZeroActivity.java
@@ -74,7 +74,7 @@ public class ZeroActivity extends Activity {
         root.addView(swBack);
 
         Switch swBB = new Switch(this);
-        swBB.setText("Banner Block (expiremental)  ");
+        swBB.setText("Banner Block (expiremental & may not work)  ");
         swBB.setTextColor(Color.WHITE);
         swBB.setTextSize(20);
         swBB.setChecked(bannerBlock);
@@ -83,7 +83,7 @@ public class ZeroActivity extends Activity {
         root.addView(swBB);
 
         Switch swVBS = new Switch(this);
-        swVBS.setText("Video Ads Skip (expiremental)  ");
+        swVBS.setText("Video Ads Skip (expiremental & may not work)  ");
         swVBS.setTextColor(Color.WHITE);
         swVBS.setTextSize(20);
         swVBS.setChecked(VideoAdsSkip);
