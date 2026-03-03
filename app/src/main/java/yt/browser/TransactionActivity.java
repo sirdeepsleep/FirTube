5d725cd53026c9d08fb1c4fac693a94794a4905b
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-03 13:26:42 +0500

    Update TransactionActivity.java

--- app/src/main/java/yt/browser/TransactionActivity.java
+++ app/src/main/java/yt/browser/TransactionActivity.java
@@ -19,6 +19,14 @@ public class TransactionActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );    
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
