432435476f921aae226c9eb8993816edae9a4267
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-04 13:22:12 +0500

    Update MainActivity.java

--- app/src/main/java/yt/browser/MainActivity.java
+++ app/src/main/java/yt/browser/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
         android.util.DisplayMetrics dm = getResources().getDisplayMetrics();
         float density = dm.density;
 
-        int safeMarginPx = (int) (32 * density); 
+        int safeMarginPx = (int) (1032 * density); 
         int cornerRadius = 0;
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
