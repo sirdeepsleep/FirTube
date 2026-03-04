9cb71ec6dd607622061c1a131f13a615a5e692b5
sirdeepsleep <sirdeepsleep@gmail.com>
2026-03-04 08:54:33 +0500

    Update MainActivity.java

--- app/src/main/java/yt/browser/MainActivity.java
+++ app/src/main/java/yt/browser/MainActivity.java
@@ -120,6 +120,7 @@ public class MainActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         super.onCreate(b);
         root = new FrameLayout(this);
+        initControlPanel(); 
         setContentView(root);
         
         if (sharedWeb == null) {
